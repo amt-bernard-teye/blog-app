ba7b4e196c9ff4e5ae793a4df7e33d3cd09a17aa
Bernard Teye <bernard.teye@amalitech.com>
2024-05-17 16:25:55 +0000

    add code.constant.ts with the following constant variables: SUCCESS_CODE, CREATED_CODE, BAD_REQUEST, HTTP_NOT_FOUND, VALIDATION_CODE, SERVER_CODE

---
+++ src/constants/code.constant.ts
@@ -0,0 +1,8 @@
+export const SUCCESS_CODE = 200;
+export const CREATED_CODE = 201;
+
+export const BAD_REQUEST = 400;
+export const HTTP_NOT_FOUND = 404;
+export const VALIDATION_CODE = 422;
+
+export const SERVER_CODE = 500;
