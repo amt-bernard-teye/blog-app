3a8e7c625d53f3ffa940081f1db41ed2aa6b6ffe
Bernard Teye <bernard.teye@amalitech.com>
2024-05-17 16:24:01 +0000

    add APP_AUTH_SECRET, MAIL_PORT, MAIL_HOST, MAIL_ADDRESS, MAIL_PASSWORD, ADMIN_NAME, ADMIN_ADDRESS, ADMIN_PASSWORD constant values to env.constant.ts file

--- src/constants/env.constant.ts
+++ src/constants/env.constant.ts
@@ -1,2 +1,12 @@
 export const APP_PORT = process.env.APP_PORT;
+export const APP_AUTH_SECRET = process.env.APP_AUTH_SECRET;
+
+export const MAIL_PORT = process.env.MAIL_PORT;
+export const MAIL_HOST = process.env.MAIL_HOST;
+export const MAIL_ADDRESS = process.env.MAIL_ADDRESS;
+export const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
+
+export const ADMIN_NAME = process.env.ADMIN_NAME;
+export const ADMIN_ADDRESS = process.env.ADMIN_ADDRESS;
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
