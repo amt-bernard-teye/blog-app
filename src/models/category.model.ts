ed0db218eaae7a664cd82792769b8e6fc483d7b3
Bernard Teye <bernard.teye@amalitech.com>
2024-05-20 10:45:04 +0000

    add CategoryModel class extending Database and implementing ISingleFinders and IMultipleFinders containing the following functions selectProps, insert, update, delete, find, findByName, findAll and countRows

---
+++ src/models/category.model.ts
@@ -0,0 +1,118 @@
+import { UsageStatus } from "@prisma/client";
+import { Category, CategoryProp } from "../types/category.type";
+import { IMultipleFinders, ISingleFinder } from "../types/finders.type";
+import { Database } from "./database.model";
+
+export class CategoryModel extends Database<Category, CategoryProp> 
+    implements ISingleFinder<Category>, IMultipleFinders<Category> {
+    protected selectProps(): CategoryProp {
+        return {
+            id: true,
+            name: true
+        };
+    }
+
+    async insert(entity: Category): Promise<Category> {
+        const prisma = this.open();
+        
+        const addedCategory = await prisma.category.create({
+            data: {
+                name: entity.name
+            },
+            select: this.selectProps()
+        });
+
+        await this.close();
+        return addedCategory;
+    }
+
+    async update(entity: Category): Promise<Category> {
+        const prisma = this.open();
+        
+        const updatedCategory = await prisma.category.update({
+            where: {
+                id: entity.id!
+            },
+            data: {
+                name: entity.name,
+                status: entity.status
+            }
+        });
+
+        await this.close();
+        return updatedCategory;
+    }
+
+    async delete(entityId: string | number): Promise<void> {
+        const prisma = this.open();
+
+        await prisma.category.update({
+            where: {
+                id: +entityId
+            },
+            data: {
+                status: UsageStatus.HIDDEN
+            }
+        });
+        
+        await this.close();
+    }
+
+    async find(entityId: number | string): Promise<Category | null> {
+        const prisma = this.open();
+
+        const foundCategory = await prisma.category.findFirst({
+            where: {
+                id: +entityId,
+                status: UsageStatus.ACTIVE
+            }
+        });
+
+        await this.close();
+        return foundCategory;
+    }
+
+    async findByName(entityName: string): Promise<Category | null> {
+        const prisma = this.open();
+
+        const foundCategory = await prisma.category.findFirst({
+            where: {
+                name: {
+                    contains: entityName,
+                    mode: "insensitive"
+                }
+            }
+        });
+
+        await this.close();
+        return foundCategory;
+    }
+
+    async findAll(page: number, rows: number): Promise<Category[]> {
+        const prisma = this.open();
+
+        const categories = await prisma.category.findMany({
+            skip: page,
+            take: rows,
+            where: {
+                status: UsageStatus.ACTIVE
+            },
+            select: this.selectProps()
+        });
+
+        await this.close();
+        return categories;
+    }
+
+    async countRows(): Promise<number> {
+        const prisma = this.open();
+        const totalCategories = await prisma.category.count({
+            where: {
+                status: UsageStatus.ACTIVE
+            }
+        });
+
+        await this.close();
+        return totalCategories;
+    }
+}
