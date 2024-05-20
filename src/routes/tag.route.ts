8f4d1efd26bdb49442b23cbdcb38572bc3072e8a
Bernard Teye <bernard.teye@amalitech.com>
2024-05-20 13:32:44 +0000

    add tag.route.ts containing route for creating, fetching, updating and deleting tags

---
+++ src/routes/tag.route.ts
@@ -0,0 +1,49 @@
+import { Router } from "express";
+
+import { ValidateTokenMiddleware } from "../middlewares/validate-token.middleware";
+import { TagController } from "../controllers/tag.controller";
+import { validateText } from "../validators/general.validator";
+import { TagModel } from "../models/tag.model";
+import { EnsureRoleIsAdminMiddleware } from "../middlewares/ensure-role-admin.middleware";
+import { RequestMiddleware } from "../middlewares/request.middleware";
+import { validateTagId } from "../validators/tag.validator";
+
+export const tagRoute = Router();
+
+tagRoute.get(
+    "/tags",
+    ValidateTokenMiddleware.run,
+    TagController.index
+);
+
+tagRoute.post(
+    "/tags",
+    EnsureRoleIsAdminMiddleware.run,
+    validateText("name")
+        .custom(async (value) => {
+            const model = new TagModel();
+            const existingTag = await model.find(value);
+            if (existingTag) {
+                throw new Error("No duplicates allowed");
+            }
+            return true
+        }).escape(),
+    TagController.store
+);
+
+tagRoute.put(
+    "/tags/:id",
+    EnsureRoleIsAdminMiddleware.run,
+    validateTagId(),
+    validateText("name").escape(),
+    RequestMiddleware.run,
+    TagController.update
+);
+
+tagRoute.delete(
+    "/tags/:id",
+    EnsureRoleIsAdminMiddleware.run,
+    validateTagId(),
+    RequestMiddleware.run,
+    TagController.destroy
+);
