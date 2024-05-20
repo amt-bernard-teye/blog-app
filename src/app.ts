64fe3474e7412ec4744f75c61e4d0bb98d94df83
Bernard Teye <bernard.teye@amalitech.com>
2024-05-20 10:57:34 +0000

    use categoryRoute from category.route.ts in app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import { APP_PORT } from "./constants/env.constant";
 import { DatabaseSeeder } from "./seeders/database.seeder";
 import { swaggerConfiguration } from "./swagger/config.swagger";
 import { authRoute } from "./routes/auth.route";
+import { categoryRoute } from "./routes/category.route";
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, "uploads")));
 app.use("/api-docs", swagger.serve, swagger.setup(swaggerConfiguration));
 
 app.use("/api", authRoute);
+app.use("/api", categoryRoute);
 
 app.listen(APP_PORT, () => {
     DatabaseSeeder.run();
