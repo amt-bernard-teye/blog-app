c8fdd276a8fcb865db1bd5c2e18fce771b5b67c3
Bernard Teye <bernard.teye@amalitech.com>
2024-05-20 10:57:05 +0000

    add category.route.ts containing route for creating, updating, fetching and delete categories

---
+++ src/routes/category.route.ts
@@ -0,0 +1,52 @@
+import { Router } from "express";
+
+import { CategoryController } from "../controllers/category.controller";
+import { CategoryModel } from "../models/category.model";
+import { validateText } from "../validators/general.validator";
+import { RequestMiddleware } from "../middlewares/request.middleware";
+import { validateCategoryId } from "../validators/category.validator";
+import { ValidateTokenMiddleware } from "../middlewares/validate-token.middleware";
+import { EnsureRoleIsAdminMiddleware } from "../middlewares/ensure-role-admin.middleware";
+
+const categoryRoute = Router();
+
+categoryRoute.get(
+    "/categories",
+    ValidateTokenMiddleware.run,
+    CategoryController.index
+);
+
+categoryRoute.post(
+    "/categories",
+    EnsureRoleIsAdminMiddleware.run,
+    validateText("name")
+        .custom(async (value) => {
+            const categoryModel = new CategoryModel();
+            const existingCategory = await categoryModel.findByName(value);
+            if (existingCategory) {
+                throw new Error("No duplicates allowed");
+            }
+            return true;
+        }).escape(),
+    RequestMiddleware.run,
+    CategoryController.store
+);
+
+categoryRoute.put(
+    "/categories/:id",
+    EnsureRoleIsAdminMiddleware.run,
+    validateCategoryId(),
+    validateText("name").escape(),
+    RequestMiddleware.run,
+    CategoryController.update
+);
+
+categoryRoute.delete(
+    "/categories/:id",
+    EnsureRoleIsAdminMiddleware.run,
+    validateCategoryId(),
+    RequestMiddleware.run,
+    CategoryController.destroy
+);
+
+export { categoryRoute };
