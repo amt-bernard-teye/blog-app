39977031cdc5595bba083304745649fedde7eb66
Bernard Teye <bernard.teye@amalitech.com>
2024-05-20 10:54:52 +0000

    add validate-token.middleware.ts containing a ValidateTokenMiddleware class containing a static run() to run the validation on the provided authorization token

---
+++ src/middlewares/validate-token.middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+
+import { UNAUTHORIZED_CODE } from "../constants/code.constant";
+import { UserModel } from "../models/user.model";
+import { validateAuthorizationToken } from "../validators/authorization-token.validator";
+
+export abstract class ValidateTokenMiddleware {
+    static async run(request: Request, response: Response, next: NextFunction) {
+        try {
+            const data = await validateAuthorizationToken(request);
+            const userModel = new UserModel();
+            const existingUser = await userModel.find(data.id);
+            
+            if (!existingUser) {
+                throw new Error("Access denied");
+            }
+
+            next();
+        }
+        catch(error) {
+            return response.status(UNAUTHORIZED_CODE).json({
+                message: "Access denied"
+            });
+        }
+    }
+}
