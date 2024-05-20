6102df455b36cae6024e251c51a259617a32625d
Bernard Teye <bernard.teye@amalitech.com>
2024-05-20 10:56:01 +0000

    add ensure-role-admin.middleware.ts containing EnsureRoleAdminMiddleware class with a static run() to check if the user is an admin

---
+++ src/middlewares/ensure-role-admin.middleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+
+import { UNAUTHORIZED_CODE } from "../constants/code.constant";
+import { UserModel } from "../models/user.model";
+import { Role } from "@prisma/client";
+import { validateAuthorizationToken } from "../validators/authorization-token.validator";
+
+export abstract class EnsureRoleIsAdminMiddleware {
+    static async run(request: Request, response: Response, next: NextFunction) {
+        try {
+            const data = await validateAuthorizationToken(request);
+            const userModel = new UserModel();
+            const existingUser = await userModel.find(data.id);
+
+            if (!(existingUser && existingUser.role === Role.ADMIN)) {
+                throw new Error("Access denied");
+            }
+
+            next();
+        }
+        catch(error) {
+            return response.status(UNAUTHORIZED_CODE).json({
+                message: "Access denied"
+            });
+        }
+    }
+} 
