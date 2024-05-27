9acbbc09a82b9c9c8ef338cc44be447b75ec6c2b
Bernard Teye <bernard.teye@amalitech.com>
2024-05-27 08:50:39 +0000

    add more http header codes to code.constant.ts

--- src/constants/code.constant.ts
+++ src/constants/code.constant.ts
@@ -4,6 +4,6 @@ export const CREATED_CODE = 201;
 export const BAD_REQUEST = 400;
 export const HTTP_NOT_FOUND = 404;
 export const VALIDATION_CODE = 422;
-export const UNAUTHORIZED_CODE = 403;
+export const UNAUTHORIZED_CODE = 401;
 
 export const SERVER_CODE = 500;
